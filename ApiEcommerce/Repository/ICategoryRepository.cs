5ca6f0dd6a778ee9fa4e6dff3edc4f83f084fb18
fperez15 <francis.perez1590@gmail.com>
2026-04-19 17:35:56 -0500

    Interface ICategoryRepository

---
+++ ApiEcommerce/Repository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ApiEcommerce.Repository;
+
+public interface ICategoryRepository
+{
+    ICollection<Category> GetCategories();
+    Category GetCategory(int id);
+    bool CategoryExists(int id);
+    bool CategoryExists(string id);
+    bool CreateCategory(Category category);
+    bool UpdateCategory(Category category);
+    bool DeleteCategory(Category category);
+    bool Save();
+}
